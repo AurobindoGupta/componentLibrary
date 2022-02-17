07399e732aa98a8a670dc8e7bcdba6e8c5782390
AurobindoGupta <aurogpt10@gmail.com>
2022-02-18 02:03:48 +0530

    card

---
+++ Documentation/card/script.js
@@ -0,0 +1,7 @@
+let ele = document.querySelectorAll(".icon-close");
+
+ele.forEach(item=> item.addEventListener("click", function (e) {
+  if (e.target.className) {
+    this.parentElement.parentElement.style.display = "none";
+  }
+}));
