d7bdbb304d87abeb855e5f919d5d88c964877577
AurobindoGupta <aurogpt10@gmail.com>
2022-02-17 12:53:55 +0530

    clean up

--- Components/card/script.js
+++ Components/card/script.js
@@ -1,4 +1,4 @@
-var ele = document.querySelectorAll(".icon-close");
+let ele = document.querySelectorAll(".icon-close");
 
 ele.forEach(item=> item.addEventListener("click", function (e) {
   if (e.target.className) {
